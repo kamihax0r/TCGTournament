6d7ce142c977dbcd9ddecc206b3770d7e1023fe0
kamihax0r <anothermatthewmiller@gmail.com>
2018-02-12 16:59:10 -0500

    First commit (maybe)

---
+++ Address.js
@@ -0,0 +1,8 @@
+//Locations and People can both have addresses
+function Address() {
+    this.street1 = "";
+    this.street2 = "";
+    this.zip = 0;
+    this.state = "";
+    this.city = "";
+}
---
+++ IDNumbers.js
@@ -0,0 +1,4 @@
+//A class to store all of a players ID numbers for playing in different kinds of tournaments
+function IDNumbers() {
+    this.DCINumber = null;
+}
